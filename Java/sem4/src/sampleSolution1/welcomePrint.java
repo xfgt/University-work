530a67a496bb9291afbbdfa4abb007436ff5f2ee
xfgt <waffle_man562@yahoo.com>
2025-02-05 12:12:43 +0200

    introducing java

---
+++ Java/sem4/src/sampleSolution1/welcomePrint.java
@@ -0,0 +1,7 @@
+package sampleSolution1;
+
+public class welcomePrint {
+    public static void main(String[] args) {
+        System.out.println("This is a representative of sample project 1!");
+    }
+}
