596c59bad2525560ae0208c5f6302de3bee0bdcc
xfgt <waffle_man562@yahoo.com>
2025-02-05 12:18:23 +0200

    added quotations

--- Java/sem4/src/sampleSolution1/welcomePrint.java
+++ Java/sem4/src/sampleSolution1/welcomePrint.java
@@ -2,6 +2,6 @@ package sampleSolution1;
 
 public class welcomePrint {
     public static void main(String[] args) {
-        System.out.println("This is a representative of sample project 1!");
+        System.out.println("This is a \"representative\" of sample project 1!");
     }
 }
