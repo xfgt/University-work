88612bec342af34b2991d01d04b26f0a0a4f4d35
xfgt <waffle_man562@yahoo.com>
2025-02-10 12:12:02 +0200

    exam exercise 2 and file streams

--- Semester 4/C-CPP/preparations/oop/exam1/1/main.cpp
+++ Semester 4/C-CPP/preparations/oop/exam1/1/main.cpp
@@ -2,20 +2,26 @@
 
 #include "./src/Stoka.h"
 #include <iostream>
+#include <fstream>
 
 
 
 int main(){
+ 
+ 	Stoka s[5];
+ 	for(int i = 0; i < 5; i++)
+ 		std::cin >> s[i];
+ 	
 
-	Stoka s[5];
-	for(int i = 0; i < 5; i++){
-		std::cin >> s[i];
+	std::ofstream ofs;
+	ofs.open("output"); 
+	if(!ofs){
+		std::cout << "File wasn't created!"; return -1;
 	}
-	// todo read form file
-	for(int i = 0; i < 5; i++){
-		std::cout << s[i] << std::endl;
-	}
 
-
+	for(int i = 0; i < 5; i++)
+		ofs << s[i] << std::endl;
+ 	
+ 	
 	return 0;
 }
