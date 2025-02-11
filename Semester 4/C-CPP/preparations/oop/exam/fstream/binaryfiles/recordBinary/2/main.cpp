f90ca54cf196ae9ae68c9e278570a8b2ac6b4112
xfgt <waffle_man562@yahoo.com>
2025-02-11 17:34:22 +0200

    record binary read write

--- Semester 4/C-CPP/preparations/oop/exam/fstream/binaryfiles/recordBinary/2/main.cpp
+++ Semester 4/C-CPP/preparations/oop/exam/fstream/binaryfiles/recordBinary/2/main.cpp
@@ -1,73 +1,136 @@
 #include <iostream>
 #include <fstream>
-#include <string.h>
+#include <cstring>
+#include <string>
+
+
 
 
 class H{
+
 	friend std::ostream& operator << (std::ostream&, H&);
 	friend std::istream& operator >> (std::istream&, H&);
+	
 
 	private:
-		char* a; // this memory allocation
+
+		char* a;
 		int b;
 		float c;
 
+
 	public:
-		H(const char* x = "", int y =0, float z=0.){
-			a = new char[strlen(x)+1];
-			strcpy(a, x);
 
-			b = y;
-			c = z;
-
+		H(){
+			a = nullptr;
+			b = 0;
+			c = 0.f;
 		}
 
-		void seta(char* q){
-			if(a != nullptr) delete[] a; // empty *this on reach
-			a = new char[strlen(q) + 1];
-			strcpy(a, q);
+//		~H(){
+//			if(a != nullptr && strlen(a) != 0){
+//					delete[] a;
+//			}
+//		}
+
+		void  seta (char* q){
+			if(a != nullptr){
+				delete[] a;
+			}
+					
+			a = new char[10+1];
+			if(strlen(q) > 10){	
+				strncpy(a, q, 10);
+			}	
+			else{
+				strcpy(a, q);
+			}
+
 		}
-		char* geta() {return a;}
+		char* geta() { return a; }
 
 
-		void setb(int w) { b = w;}
-		int getb()	{return b;}
+		void setb(int w) { b = w; }
+		int getb() 	{ return b; }
 
-		void setc(float e) {c = e;}
-		float getc() {return c;}
+		void setc(float e) { c = e; }
+		float getc() 	{ return c;}
+
 
 
 };
 
 
-std::ostream& operator << (std::ostream& out, H& obj)
-{
+std::ostream& operator << (std::ostream& out , H& obj){
 	out << obj.geta() << std::endl
 		<< obj.getb() << std::endl
 		<< obj.getc() << std::endl;
+
+
 	return out;
 }
 std::istream& operator >> (std::istream& in, H& obj){
 
-	char* x;
-	in >> x;
 
+	char x[100];
+	in >> x;
 	obj.seta(x);
+
+	in.clear();
+	in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');	
 	in >> obj.b >> obj.c;
 
 	return in;
 }
+	
 
 
+void writeToBinFile(std::string name, H* objArr, int size){
+	
+	std::fstream ofs;
+	ofs.open(name, std::ios::out | std::ios::binary);
+	
+	if(!ofs){
+		std::cout << "Error creating file!\n"; return;
+	}
+		
+
+
+	ofs.write(reinterpret_cast<char*>(objArr), size * sizeof(H));
+	ofs.close();
+}
+
+void readBinFile(std::string name, int size){
+	
+	std::fstream ifs(name, std::ios::in | std::ios::binary);
+	if(!ifs){
+		std::cout << "Error opening file!\n"; return;
+	}
+	
+	H h[size];
+	ifs.read(reinterpret_cast<char*>(h), size * sizeof(H));
+	ifs.close();
+
+	for(H& g : h)
+		std::cout << g << std::endl;
+}
 
 
 
 int main(){
+		
+	const int NUMBER_OBJECTS = 5;
 
-	H h;
-	std::cin >> h;
-	std::cout << h << std::endl;
 
+	H h[NUMBER_OBJECTS];
+
+	for(int i = 0; i < NUMBER_OBJECTS; i++)
+		std::cin >> h[i];
+	
+
+	writeToBinFile("output.bin", h, NUMBER_OBJECTS);
+
+	readBinFile("output.bin", NUMBER_OBJECTS);
+	
 	return 0;
 }
-
