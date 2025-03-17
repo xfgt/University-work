ae7e7241e36f556fc163588f494d905ef50d8a0a
xfgt <waffle_man562@yahoo.com>
2025-03-17 12:50:24 +0200

    input constraints and variables

--- Semester 4/Java/sem4/eclipse/Course408/src/homeworks/hw3/Solution.java
+++ Semester 4/Java/sem4/eclipse/Course408/src/homeworks/hw3/Solution.java
@@ -1,5 +1,10 @@
 package homeworks.hw3;
 
+
+
+import java.util.Scanner;
+
+
 public class Solution {
 	
 	
@@ -7,18 +12,31 @@ public class Solution {
 		double deliveryPrice();
 	}
 	
+	
+	
+	
 	class DeliverableItem implements IFDeliverable{
 		
 		Boolean atAddress;
 		double extraFee;
+		
+		
 		
 		public DeliverableItem(Boolean atAddress) {
 			this.atAddress = atAddress;	
 		}
 		
 		void setExtraFee(double amount) {
-			if(atAddress)
-				extraFee = amount;
+			
+			if(atAddress) {
+				if(amount > 0)
+					extraFee = amount;
+				else 
+					extraFee = 1;
+			}
+				
+			
+			
 		}
 		
 		@Override
@@ -44,7 +62,10 @@ public class Solution {
 		
 		public Document(double price, Boolean atAddress) {
 			super(atAddress);
-			minimalDeliveryPrice = price;
+			if(price > 0)
+				minimalDeliveryPrice = price;
+			else
+				minimalDeliveryPrice = 1;
 		}
 		
 		@Override
@@ -67,8 +88,16 @@ public class Solution {
 		public WeightedItem(Material material, double weight, double price, Boolean atAddress) {
 			super(atAddress);
 			this.material = material;
-			this.weight = weight;
-			_wDeliveryPrice = price;
+			
+			if(price > 0 && weight > 0) {
+				this.weight = weight;
+				_wDeliveryPrice = price;
+			} else {
+				this.weight = 1;
+				_wDeliveryPrice = 1;
+				
+			}
+				
 		}
 		
 		@Override
@@ -80,6 +109,8 @@ public class Solution {
 				_wDeliveryPrice += super.deliveryPrice();
 			}
 			
+			
+			// 1% increase if fragile
 			if(material.isMaterialFragile()) {
 				_wDeliveryPrice = _wDeliveryPrice - _wDeliveryPrice * 0.01;
 				//(wdp -= wdp * 0.01)
@@ -90,9 +121,33 @@ public class Solution {
 	}
 		
 		
+	static Boolean toBooleanValue(int x) {
+		return (x == 1 ? true : false);
 	}
 	
+	
 	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		String materialName = scanner.nextLine();
+		
+		int holdIsFragile = Integer.parseInt(scanner.nextLine());
+		Boolean isFragile = toBooleanValue(holdIsFragile);
+		int holdToAddress= Integer.parseInt(scanner.nextLine());
+		Boolean toAddress = toBooleanValue(holdToAddress);
+		
+		double minPrice = Double.parseDouble(scanner.nextLine());
+		double weight = Double.parseDouble(scanner.nextLine());
+		double pricePerKg = Double.parseDouble(scanner.nextLine());
+		double additionalPrice = Double.parseDouble(scanner.nextLine());
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		return;
