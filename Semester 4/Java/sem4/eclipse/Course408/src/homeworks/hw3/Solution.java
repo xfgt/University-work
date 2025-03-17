06f4aac6cbbdfc941365d27325b5195c192b26d3
workbook <f113621@students.nbu.bg>
2025-03-17 05:01:11 +0200

    hw3 boilerplate

--- Semester 4/Java/sem4/eclipse/Course408/src/homeworks/hw3/Solution.java
+++ Semester 4/Java/sem4/eclipse/Course408/src/homeworks/hw3/Solution.java
@@ -2,7 +2,98 @@ package homeworks.hw3;
 
 public class Solution {
 	
+	
+	interface IFDeliverable{
+		double deliveryPrice();
+	}
+	
+	class DeliverableItem implements IFDeliverable{
+		
+		Boolean atAddress;
+		double extraFee;
+		
+		public DeliverableItem(Boolean atAddress) {
+			this.atAddress = atAddress;	
+		}
+		
+		void setExtraFee(double amount) {
+			if(atAddress)
+				extraFee = amount;
+		}
+		
+		@Override
+		public double deliveryPrice() {
+			return extraFee;
+		}
+	}
+	
+	class Material{
+		String name;
+		Boolean isFragile;
+		
+		public Material(String name, Boolean isFragile) {
+			this.name = name;
+			this.isFragile = isFragile;
+		}
+		
+		public Boolean isMaterialFragile() { return isFragile; }
+	}
+	
+	class Document extends DeliverableItem implements IFDeliverable{
+		double minimalDeliveryPrice;
+		
+		public Document(double price, Boolean atAddress) {
+			super(atAddress);
+			minimalDeliveryPrice = price;
+		}
+		
+		@Override
+		public double deliveryPrice(){
+			if(super.atAddress) {
+				minimalDeliveryPrice += super.deliveryPrice();
+			}
+			return minimalDeliveryPrice;
+		}
+		
+	
+	}
+	
+	class WeightedItem extends DeliverableItem implements IFDeliverable{
+		
+		Material material;
+		double weight;
+		double _wDeliveryPrice;
+		
+		public WeightedItem(Material material, double weight, double price, Boolean atAddress) {
+			super(atAddress);
+			this.material = material;
+			this.weight = weight;
+			_wDeliveryPrice = price;
+		}
+		
+		@Override
+		public double deliveryPrice() {
+			
+			_wDeliveryPrice *= weight;
+			
+			if(super.atAddress) {
+				_wDeliveryPrice += super.deliveryPrice();
+			}
+			
+			if(material.isMaterialFragile()) {
+				_wDeliveryPrice = _wDeliveryPrice - _wDeliveryPrice * 0.01;
+				//(wdp -= wdp * 0.01)
+			}
+			return _wDeliveryPrice;
+		}
+		
+	}
+		
+		
+	}
+	
 	public static void main(String[] args) {
+		
 		
 		return;
 		
