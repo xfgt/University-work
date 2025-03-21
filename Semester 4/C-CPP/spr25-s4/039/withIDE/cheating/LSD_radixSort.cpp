9669f593e20e19a002621dc3a2c1cd6598223204
xfgt <waffle_man562@yahoo.com>
2025-03-21 13:38:54 +0200

    radix sort: the n*m version of counting sort

---
+++ Semester 4/C-CPP/spr25-s4/039/withIDE/cheating/LSD_radixSort.cpp
@@ -0,0 +1,28 @@
+
+
+// A utility function to get maximum
+// value in arr[]
+int getMax(int arr[], int n)
+{
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+
+
+void radixsort(int arr[], int n)
+{
+
+    // Find the maximum number to
+    // know number of digits
+    int m = getMax(arr, n);
+
+    // Do counting sort for every digit.
+    // Note that instead of passing digit
+    // number, exp is passed. exp is 10^i
+    // where i is current digit number
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
