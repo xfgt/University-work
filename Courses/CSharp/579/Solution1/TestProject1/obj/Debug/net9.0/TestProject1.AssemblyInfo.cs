9aea333e2c058b2ad32b3ddd1a626012a59f25e2
xfgt <waffle_man562@yahoo.com>
2025-10-05 12:06:25 +0300

    inner gitignores

--- Courses/CSharp/579/Solution1/TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
+++ Courses/CSharp/579/Solution1/TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86b657a83b2c0ea18a6fcfba8d6c579a309bd0de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41aedbdca2bdccfa52878187853879482cad20e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
