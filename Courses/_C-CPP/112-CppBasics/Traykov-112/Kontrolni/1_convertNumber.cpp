820638df178ce62eb37777dce7350ebed4ef8521
xfgt <waffle_man562@yahoo.com>
2026-03-17 17:46:42 +0200

    finish conversions

--- Courses/_C-CPP/112-CppBasics/Traykov-112/Kontrolni/1_convertNumber.cpp
+++ Courses/_C-CPP/112-CppBasics/Traykov-112/Kontrolni/1_convertNumber.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include <cmath>
+#include <cstring>
 
 using namespace std;
-
-
-
-
 
 
 
@@ -14,7 +11,7 @@ void printArr(const int arr[], const int sz) {
 		cout << arr[i];
 	cout << '\n';
 }
-void printArr(const char arr[], const int sz) {
+void printCArr(const char arr[], const int sz) {
 	for (int i = 0; i < sz; i++)
 		cout << arr[i];
 	cout << '\n';
@@ -89,6 +86,94 @@ void toHex(int n) {
 }
 
 
+int fromBinToDec(char num[]) {
+
+	// const size = 6
+	//32 16 8 4 2 1
+	int sum{};
+	int mul = 32;
+	for (int i = 0; i < 6; i++) {
+
+		if (mul != 0 && num[i] == '1') {
+			sum += mul;
+		}
+
+		mul /= 2;
+	}
+
+	return sum;
+}
+
+
+double fromHexToDec(char num[]) {
+
+	// const sz
+	const int size = strlen(num);
+
+
+
+	double sum{};
+	double converted{};
+	for (int i = size-1, j = 0; i >= 0; --i, j++) {
+
+
+			switch (num[i]) {
+				default:
+					converted = num[i] - '0';
+					sum += converted * pow(16, j);
+					break;
+
+				case 'A':
+				case 'a':
+					sum += (10 * pow(16, j));
+					break;
+				case 'B':
+				case 'b':
+					sum += (11 * pow(16, j));
+					break;
+				case 'C':
+				case 'c':
+					sum += (12 * pow(16, j));
+					break;
+				case 'D':
+				case 'd':
+					sum += (13 * pow(16, j));
+					break;
+				case 'E':
+				case 'e':
+					sum += (14 * pow(16, j));
+					break;
+				case 'F':
+				case 'f':
+					sum += (15 * pow(16, j));
+					break;
+			}
+
+
+	}
+
+	return sum;
+}
+
+
+void fromOctToBin(char num[]) {
+
+	const int size = strlen(num)-1;
+
+	int sum{};
+
+	for (int i = 0, j = size; i <= size; i++, j--) {
+		int converted = num[i] - '0';
+		sum += converted * pow(8, j);
+
+	}
+
+
+
+	cout << sum << endl;
+	toBinary(sum);
+	//nakraq to binary nested
+}
 
 int main() {
 
@@ -100,17 +185,35 @@ int main() {
 	cout << hex << n << endl;
 	
 	cout << endl;
-	// binary 101101
-	
-	
-	toBinary(n);	
-	toOct(n);
-	toHex(n);
+
+	// toBinary(n);
+	// toOct(n);
+	// toHex(n);
 
 
 	// 2. Преобразувайте числото 101101₂ в десетична бройна система. (2 т.)
+
+	int a = fromBinToDec("111111");
+	cout << dec <<  a << endl;
+
+
+
+
 	// 3. Преобразувайте числото 2F₁₆ в десетична бройна система. (2 т.)
+	a = fromHexToDec("2F"); // 2F, F2
+	cout << dec << a << endl;
+
+	a = fromHexToDec("F2"); // 2F, F2
+	cout << dec << a << endl;
+
+	double b = fromHexToDec("BABACECA"); // 2F, F2
+	cout << dec << fixed << b << endl;
+
+
 	// 4. Преобразувайте числото 57₈ в двоична бройна система. (3 т.)
+	fromOctToBin("57");
+	fromOctToBin("173");
+	fromOctToBin("2162");
 
 
 	return 0;
