86e16a3984926e343c52ca159d9d8879866acab7
xfgt <waffle_man562@yahoo.com>
2026-03-19 06:44:37 +0200

    recursion and stack filling demonstration

--- Courses/_C-CPP/112-CppBasics/Traykov-112/Arrays and pointers/4-0_MemoryAllocation.cpp
+++ Courses/_C-CPP/112-CppBasics/Traykov-112/Arrays and pointers/4-0_MemoryAllocation.cpp
@@ -7,33 +7,59 @@
 #include <thread>
 #include <chrono>
 
+
+void demonstrateStack() {
+    static int x = 0;
+    std::cout << reinterpret_cast<void*>(demonstrateStack) << ":\t" << x << std::endl;
+    if (x == 10) {
+        std::cout << "Stack has reached size of 10!\nWe're backing up.." << std::endl;
+        return;
+    }
+    x++;
+    demonstrateStack(); // recursion
+    x--;
+    std::cout << reinterpret_cast<void*>(demonstrateStack) << ":\t" << x << std::endl;
+}
+
 int main() {
 
     // On the stack: limited size; LIFO - Last in first out;
+    // https://web.stanford.edu/class/archive/cs/cs107/cs107.1258/lectures/7/Lecture7.pdf
     int a;
     //long double ld[ 1000000 ]; // error on run time: Signal: SIGSEGV (Segmentation fault) (stack overflow).
-
+    demonstrateStack();
 
     // Heap
 
-    // THE RAM FILLER
-    // int x = 10;
-    // unsigned long long* ptr;
-    // while (true) {
-    //     ptr = new unsigned long long[x]{};
-    //     std::cout << x << std::endl;
-    //     std::this_thread::sleep_for(std::chrono::seconds(1));
-    //
-    //     x *= 5;
-    // }
+    // Filling the ram up (pt. 1)
+    {
+        unsigned long long* asdf = new unsigned long long[999999999]{};
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        delete[] asdf;
+    }
 
 
 
 
-    //delete[] ptr;
 
-    unsigned long long* asdf = new unsigned long long[999999999]{};
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-    delete[] asdf;
+    //THE RAM FILLER (pt. 2)
+    {
+        int x = 10;
+        unsigned long long* ptr;
+        while (true) {
+            ptr = new unsigned long long[x]{};
+            std::cout << x << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+
+            x *= 5;
+        }
+        delete[] ptr; // wont execute because of exception
+    }
+
+
+
+
+
+
     return 0;
 }
